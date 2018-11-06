46d2715e08fbc60972a1ef1a4bc3c79237055599
Chris Keefe <crk239@nau.edu>
2018-11-05 21:03:11 -0700

    working draft of image fetch

--- js/canyonImages.js
+++ js/canyonImages.js
@@ -1,17 +1,83 @@
 // get random image
-let data = null
+const primaryImageContainer = document.querySelector(".primaryImageContainer")
+const randomTo15 = function(){return Math.floor(Math.random()*16)}
+const url = 'https://gist.githubusercontent.com/ChrisKeefe/cc60c871074ee5be03bbea73ccb45090/raw/f10fec188daf185aad16766ad1db8c47899c4a60/CanyonImages.JSON'
+const imageList = [];
 
-function getRandomImage(){
-  fetch('https://gist.githubusercontent.com/kevindeedavis/7d00db771e821d59acc3/raw/b499077bbe762f843b1ff34d02613ee94786ed89/.js', {
-    method: 'GET'
+fetch(url)
+  .then((response) => {
+    if(response.ok){
+      return response.json();
+    }
   })
-  .then(function(response) { return response.json(); })
   .then(function(json) {
-    data = json.images.length
-    console.log(data)
-    const objectIndex = Math.random() * json.images.length + 1
-    console.log(json.images[objectIndex])
-  });
+    imageList.push(...json.images);
+    //get length of imageList (pass to addFeaturesToImage)
+    generateRdmImage()
+
+  })
+
+
+function generateRdmImage() {
+  // once we have length of imageList, pass that in to this function
+  // then generate an array of ids, and pop them as we assign them to images
+  // this should prevent duplication
+  const img = document.createElement("img")
+  const id = randomTo15()
+  console.log(id)
+  img.classList.add("primaryImage")
+  img.src = imageList[id].image.src
+  img.alt = imageList[id].caption
+  primaryImageContainer.appendChild(img)
 }
+
+
+for(const image of imageList) {
+  let title = image.title
+  console.log(title);
+}
+
+
 // get image based on caption/name search
-getRandomImage()
+//NEW IMAGE button?
+//More like this button?
+//Dislike button?
+
+//maybe this thing takes user dislikes (removes them from list) and
+//preferences (shows "more like this" based on parsing of title words)
+//saves common words from liked titles, and creates a user profile?
+
+// var myImage = document.querySelector('img');
+// var myRequest = new Request('flowers.jpg');
+//
+// fetch(myRequest).then(function(response) {
+//   console.log(response.ok); // returns true if the response returned successfully
+//   response.blob().then(function(myBlob) {
+//     var objectURL = URL.createObjectURL(myBlob);
+//     myImage.src = objectURL;
+//   });
+// });
+
+// function getRandomImage(){
+// TODO: finish
+//     const objectIndex = Math.random() * json.images.length + 1
+//     console.log(json.images[objectIndex])
+//  }
+
+ /**
+  * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
+  * images to fit into a certain area.
+  * @author (Stackexchange User) Jason J. Nathan
+  * @author https://stackoverflow.com/questions/170624/javascript-image-resize#170636
+  * @param {Number} srcWidth width of source image
+  * @param {Number} srcHeight height of source image
+  * @param {Number} maxWidth maximum available width
+  * @param {Number} maxHeight maximum available height
+  * @return {Object} { width, height }
+  */
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
