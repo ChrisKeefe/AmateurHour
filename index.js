b8d0b1bd8deae27933569ca1553f4cf2af6ec1a2
Chris Keefe <crk239@nau.edu>
2018-11-26 11:17:38 -0700

    adds link to return to webform, many notes

--- index.js
+++ index.js
@@ -18,17 +18,69 @@ server.get('/webform.html', (req, res) => {
 server.post('/webform.html', (req, res) => {
   const template_vars = {
     firstname: req.body['firstname'],
+    firstnameValid: firstNameValid(),
     phone: req.body['phone'],
+    phoneValid: phoneValid(),
     email: req.body['email'],
+    emailValid: emailValid(),
     environment: req.body['environment'],
-    foodStory: req.body['foodStory']
+    environmentValid: environmentValid(),
+    foodStory: req.body['foodStory'],
+    foodStoryValid: foodStoryValid()
   }
   console.log(template_vars)
   res.render('webform', template_vars)
 })
 
+// replace server.post with :
+// server.post('/webform.html', handleFormSubmission(req, res))
+//
+// function handleFormSubmission(req, res){
+//   const template_vars = {
+//     firstname: req.body['firstname'],
+//     firstnameValid: firstNameValid(),
+//     phone: req.body['phone'],
+//     phoneValid: phoneValid(),
+//     email: req.body['email'],
+//     emailValid: emailValid(),
+//     environment: req.body['environment'],
+//     environmentValid: environmentValid(),
+//     foodStory: req.body['foodStory'],
+//     foodStoryValid: foodStoryValid()
+//   }
+//   console.log(template_vars)
+//   res.render('webform', template_vars)
+// }
+
+// Do same for server setup
+
 server.use(express.static('static'))
 server.listen(port)
+
+// TODO: alternately, check validity in a single formIsValid fn, using:
+// return ! (template_vars.name === undefined) ||
+          // template_vars.phoneValid === problematic)
+
+/// this could be saved as locals.errors and passed to ejs form template: if locals.errors{there were errors in your form}
+function firstNameValid(template_vars) {
+  return ! template_vars.firstname === undefined
+}
+
+function phoneValid(template_vars) {
+  return true;
+}
+
+function emailValid(template_vars) {
+  return true;
+}
+
+function environmentValid(template_vars) {
+  return true;
+}
+
+function foodStoryValid(template_vars) {
+  return true;
+}
 
 // TODO: add functions validating fields here
 // call them in server.post
