3630bbaae509b54031d275cfefec9760769eaa35
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2018-11-28 14:50:17 -0700

    Adds webform GET/POST intercept and validation, node.js (#5)
    * reorganizes repo for ejs
    
    * wires up GET/POST intercepts for webform
    
    * adds link to return to webform, notes, all but phone validations
    
    * Handles validation with HTML5 pattern and required attribs

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser =require('body-parser')
+const logger = require('morgan')
+const port = 8080
+
+setUpServer()
+
+server.get('/webform.html', (req, res) => {
+  res.render('webform')
+})
+
+server.post('/webform.html', handleFormSubmission)
+
+function setUpServer(){
+  server = express()
+  server.use(logger('dev'))
+  server.use(bodyParser.urlencoded({extended: true}))
+  server.set('view engine', 'ejs');
+}
+
+function handleFormSubmission(req, res){
+  const template_vars = {
+    firstname: req.body['firstname'],
+    phone: req.body['phone'],
+    email: req.body['email'],
+    environment: req.body['environment'],
+    foodStory: req.body['foodStory'],
+  }
+  res.render('webform', template_vars)
+}
+
+server.use(express.static('static'))
+server.listen(port)
