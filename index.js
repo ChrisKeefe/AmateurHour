12d1c2033f1c0b3bb02b32acc9912ee4fa5dbd40
Chris Keefe <crk239@nau.edu>
2018-11-27 21:35:12 -0700

    Handles validation with HTML5 pattern and required attribs

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const phoneValidationRegex = new RegExp("((\+|00)?[1-9]{2}|0)[1-9]( ?[0-9]){8}")
-
 const express = require('express')
 const bodyParser =require('body-parser')
 const logger = require('morgan')
 const port = 8080
 
-server = express()
-server.use(logger('dev'))
-server.use(bodyParser.urlencoded({extended: true}))
-server.set('view engine', 'ejs');
+setUpServer()
 
 server.get('/webform.html', (req, res) => {
   res.render('webform')
 })
 
-server.post('/webform.html', (req, res) => {
+server.post('/webform.html', handleFormSubmission)
+
+function setUpServer(){
+  server = express()
+  server.use(logger('dev'))
+  server.use(bodyParser.urlencoded({extended: true}))
+  server.set('view engine', 'ejs');
+}
+
+function handleFormSubmission(req, res){
   const template_vars = {
     firstname: req.body['firstname'],
     phone: req.body['phone'],
@@ -22,52 +26,8 @@ server.post('/webform.html', (req, res) => {
     environment: req.body['environment'],
     foodStory: req.body['foodStory'],
   }
-// TODO: add phone number validation, remove other validation infrastructure
-  console.log(template_vars.firstname + "template_vars")
-  const validity_check = {
-    phoneValid: phoneValid(template_vars)
-  }
-  console.log(template_vars)
   res.render('webform', template_vars)
-})
-
-// replace server.post with :
-// server.post('/webform.html', handleFormSubmission(req, res))
-//
-// function handleFormSubmission(req, res){
-//   const template_vars = {
-//     firstname: req.body['firstname'],
-//     firstnameValid: firstNameValid(),
-//     phone: req.body['phone'],
-//     phoneValid: phoneValid(),
-//     email: req.body['email'],
-//     : (),
-//     environment: req.body['environment'],
-//     environmentValid: environmentValid(),
-//     foodStory: req.body['foodStory'],
-//     foodStoryValid: foodStoryValid()
-//   }
-//   console.log(template_vars)
-//   res.render('webform', template_vars)
-// }
-
-// Do same for server setup
+}
 
 server.use(express.static('static'))
 server.listen(port)
-
-// TODO: alternately, check validity in a single formIsValid fn, using:
-// return ! (template_vars.name === undefined) ||
-          // template_vars.phoneValid === problematic)
-
-/// this could be saved as locals.errors and passed to ejs form template: if locals.errors{there were errors in your form}
-function firstNameValid(template_vars) {
-  return ! template_vars.firstname === undefined
-}
-
-function phoneValid(template_vars) {
-  return true
-}
-
-// TODO: add functions validating fields here
-// call them in server.post
