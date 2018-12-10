209a44c107f84480eac115fb2bb4a7139f6110b4
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2018-12-09 20:00:11 -0700

    Creates and wires up SQLite3 database to store form data (#6)

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express')
 const bodyParser =require('body-parser')
 const logger = require('morgan')
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/formSubmissions.db3');
 const port = 8080
 
 setUpServer()
-
-server.get('/webform.html', (req, res) => {
-  res.render('webform')
-})
-
-server.post('/webform.html', handleFormSubmission)
 
 function setUpServer(){
   server = express()
   server.use(logger('dev'))
   server.use(bodyParser.urlencoded({extended: true}))
   server.set('view engine', 'ejs');
+
+  server.get('/webform.html', (req, res) => {
+    res.render('webform')
+  })
+
+  server.get('/formSubmissions.html', (req, res) => {
+    db.all('SELECT * FROM userInfo', (err, rows) => {
+      console.log({rows: rows})
+      res.render('formSubmissions', {rows: rows})
+    })
+  })
+
+  server.post('/webform.html', handleFormSubmission)
 }
 
 function handleFormSubmission(req, res){
@@ -26,7 +35,20 @@ function handleFormSubmission(req, res){
     environment: req.body['environment'],
     foodStory: req.body['foodStory'],
   }
-  res.render('webform', template_vars)
+  saveData(template_vars)
+  res.redirect('formSubmissions.html')
+}
+
+function saveData (formData) {
+  console.log(formData)
+  const query = `INSERT INTO userInfo (firstname, phone, email, environment, foodStory) ` +
+        `VALUES ( ?, ?, ?, ?, ? )`
+  console.log(query)
+  db.run(query, [formData.firstname, formData.phone, formData.email, formData.environment, formData.foodStory])
+}
+
+function getData (){
+
 }
 
 server.use(express.static('static'))
